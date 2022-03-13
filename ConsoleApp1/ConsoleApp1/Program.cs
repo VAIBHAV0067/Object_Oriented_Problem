5c9d3aa58bf9aba69abfda13a868a6970762ed12
tutorial6 <manapure.vaibhav542@gmail.com>
2022-03-13 11:25:25 +0530

    Problem 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InventoryDataManagement
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "E:/BridgeLabz/Object_Oriented_Problem/Object_Oriented_Problem/ConsoleApp1/ConsoleApp1/json1.json";
+
+            FetchDataForRice fetchDataForRice = new FetchDataForRice();
+            Rice data = fetchDataForRice.Read(path);
+
+            Console.WriteLine(data.Name);
+            Console.WriteLine(data.Weight);
+            Console.WriteLine(data.Price);
+
+
+        }
+    }
+}
